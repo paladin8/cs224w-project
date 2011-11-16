7608451af628d9415a41ceb8c39f307c24937324
Jeffrey Wang <jeffreyw314159@gmail.com>
2011-11-16 14:34:37 -0800

    Apparently I don't know how to spell "hierarchy."

--- hnetwork.py
+++ hnetwork.py
@@ -4,8 +4,8 @@ import math;
 
 class HNetwork:
     
-    # initialize a heirarchical network with:
-    #   H   number of heirarchies
+    # initialize a hierarchical network with:
+    #   H   number of hierarchies
     #   l   levels in tree
     #   b   branching factor
     #   g   group size
@@ -19,46 +19,46 @@ class HNetwork:
         self.a = a;
         self.dd = dd;
 
-        # dimension size of each heirarchy
+        # dimension size of each hierarchy
         self.dim = pow(self.b, self.l);
         
         self.init_random_network()
     
-    # initialize random heirarchical network with the given parameters
+    # initialize random hierarchical network with the given parameters
     def init_random_network(self):
         self.G = nx.MultiDiGraph();
         self.G.add_nodes_from(xrange(self.dim * self.g));
 
-        # initialize heirarchy partitions
-        # self.heir[h][i] = list of nodes with coordinate i in heirarchy h
-        # self.heirct[h][i][j] = number of nodes under the j-th subtree
-        #                        at level i in heirarchy h (level 0 = bottom)
-        self.heir = {};
-        self.heirct = {};
+        # initialize hierarchy partitions
+        # self.hier[h][i] = list of nodes with coordinate i in hierarchy h
+        # self.hierct[h][i][j] = number of nodes under the j-th subtree
+        #                        at level i in hierarchy h (level 0 = bottom)
+        self.hier = {};
+        self.hierct = {};
         for h in xrange(self.H):
-            self.heir[h] = {};
+            self.hier[h] = {};
             for i in xrange(self.dim):
-                self.heir[h][i] = [];
+                self.hier[h][i] = [];
 
             # there are b^{l-i} subtrees at level i, initialize counts to zero
-            self.heirct[h] = {};
+            self.hierct[h] = {};
             subtrees = self.dim;
             for i in xrange(self.l):
-                self.heirct[h][i] = {};
+                self.hierct[h][i] = {};
                 for j in xrange(subtrees):
-                    self.heirct[h][i][j] = 0;
+                    self.hierct[h][i][j] = 0;
                 subtrees = subtrees / self.b;
 
-        # randomly choose heirarchies for each node
-        # average g nodes per coordinate per heirarchy
+        # randomly choose hierarchies for each node
+        # average g nodes per coordinate per hierarchy
         for s in self.G.nodes_iter():
             node_h = []
             for h in xrange(self.H):
                 i = random.randrange(self.dim);
                 node_h.append(i);
-                self.heir[h][i].append(s);
+                self.hier[h][i].append(s);
                 for j in xrange(self.l):
-                    self.heirct[h][j][i] = self.heirct[h][j][i] + 1;
+                    self.hierct[h][j][i] = self.hierct[h][j][i] + 1;
                     i = i / self.b;
             self.G.add_node(s, h = node_h);
 
@@ -74,7 +74,7 @@ class HNetwork:
             self.exp_norm = self.exp_norm + math.exp(-self.a*d);
 
     # pick random degree from degree distribution, add edges randomly
-    # by choosing random heirarchy, then random distance, then random node
+    # by choosing random hierarchy, then random distance, then random node
     def add_random_edges(self, s):
         num_edges = self.dd.rand();
         for i in xrange(num_edges):
@@ -87,7 +87,7 @@ class HNetwork:
                 t = self.random_node(s, h, d);
             self.G.add_edge(s, t);
 
-    # random heirarchical distance according to p(x) = ce^{-a*x}
+    # random hierarchical distance according to p(x) = ce^{-a*x}
     def random_dist(self):
         r = random.random();
         for d in xrange(self.l):
@@ -95,44 +95,44 @@ class HNetwork:
             if r <= 0:
                 return d;
 
-    # random node in the graph which is at distance d in heirarchy h from s
+    # random node in the graph which is at distance d in hierarchy h from s
     def random_node(self, s, h, d):
         si = self.G.node[s]['h'][h];
         if d == 0:
-            idx = random.randrange(self.heirct[h][0][si]);
+            idx = random.randrange(self.hierct[h][0][si]);
             return self.get_node(h, 0, si, idx);
 
-        # ci is the subtree at level d in heirarchy h which s is in
-        # pi is the subtree at level d-1 in heirarchy h which s is in
+        # ci is the subtree at level d in hierarchy h which s is in
+        # pi is the subtree at level d-1 in hierarchy h which s is in
         pi = si;
         for i in xrange(d-1):
             pi = pi / self.b;
         ci = pi / self.b;
 
         # number of nodes to randomly choose from, i.e. all nodes under ci
-        # which are not under pi (the ones at distance d from s in heirarchy h)
-        num_nodes = self.heirct[h][d][ci] - self.heirct[h][d-1][pi];
+        # which are not under pi (the ones at distance d from s in hierarchy h)
+        num_nodes = self.hierct[h][d][ci] - self.hierct[h][d-1][pi];
         r = random.randrange(num_nodes);
         for i in xrange(self.b):
             ni = ci * self.b + i;
             if ni != pi:
-                if r < self.heirct[h][d-1][ni]:
+                if r < self.hierct[h][d-1][ni]:
                     return self.get_node(h, d-1, ni, r);
-                r = r - self.heirct[h][d-1][ni];
+                r = r - self.hierct[h][d-1][ni];
 
-    # get the idx-th node in the ci-th subtree of level d in heirarchy h
+    # get the idx-th node in the ci-th subtree of level d in hierarchy h
     def get_node(self, h, d, ci, idx):
         if d == 0:
-            return self.heir[h][ci][idx];
+            return self.hier[h][ci][idx];
 
         # iterate over children of subtree to find which one the node is in
         for i in xrange(self.b):
             ni = ci * self.b + i;
-            if idx < self.heirct[h][d-1][ni]:
+            if idx < self.hierct[h][d-1][ni]:
                 return self.get_node(h, d-1, ni, idx);
-            idx = idx - self.heirct[h][d-1][ni];
+            idx = idx - self.hierct[h][d-1][ni];
 
-    # heirarchical distance between two nodes
+    # hierarchical distance between two nodes
     def dist(self, s, t, h):
         si = self.G.node[s]['h'][h]
         ti = self.G.node[t]['h'][h]
